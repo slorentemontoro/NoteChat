999d1951fb048548575e8fba320da7ba3922f022
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-12 11:41:24 +0200

    fix(gitignore): add angular cache

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-
-const routes: Routes = [
-=======
 import { HomeComponent } from './layouts/home/home.component';
 import { SigninComponent } from './layouts/signin/signin.component';
 
@@ -11,7 +7,6 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'signin', component: SigninComponent}
 
->>>>>>> 409309a1c23abfc600ee7a3480a8f0d83a53547f
 ];
 
 @NgModule({
