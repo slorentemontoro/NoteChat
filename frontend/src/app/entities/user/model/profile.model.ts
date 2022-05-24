3e03f734bf10975937fdfbad82335e0364379357
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-24 18:45:58 +0200

    profile changes por save image

--- frontend/src/app/entities/user/model/profile.model.ts
+++ frontend/src/app/entities/user/model/profile.model.ts
@@ -1,30 +1,36 @@
 export class Profile{
-    
+  
+    id: number
     nick:String;
     password:String;
-    rolid!: number;
     rolname: String;
+    image_user: String;
     archivos: String[];
     email?:String;
     grados?: String[];
 
 
-
   constructor(
+    id: number, 
     nick: String, 
     password: String, 
     rolname: String, 
+    image_user: String, 
     archivos: String[], 
     email?: String, 
     grados?: String[]
 ) {
+    this.id = id
     this.nick = nick
     this.password = password
     this.rolname = rolname
+    this.image_user = image_user
     this.archivos = archivos
     this.email = email
     this.grados = grados
   }
+
+
 
 
 }
