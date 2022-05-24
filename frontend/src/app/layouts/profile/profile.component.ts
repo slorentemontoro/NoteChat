43016a2ca49f7ee98af3341471a455c01261dd0f
Sergio Lorente <slorentemontoro@gmail.com>
2022-05-24 18:14:40 +0200

    change image of profile

--- frontend/src/app/layouts/profile/profile.component.ts
+++ frontend/src/app/layouts/profile/profile.component.ts
@@ -12,74 +12,35 @@ export class ProfileComponent implements OnInit {
   nombre!: string
   email!: string
   rol!: string
-  image!: string
+  image!: any
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getUserByJWT()
 
-    console.log(this.route.snapshot.paramMap.get("userId"))
   }
 
   async getUserByJWT() {
     var token = localStorage.getItem('jwt');
     var decodeToken: JSON =  jwt_decode(token!);
-    console.log(decodeToken)
     this.nombre = Object.values(decodeToken)[1].nick
+    this.nombre =  this.nombre.charAt(0).toUpperCase() + this.nombre.slice(1);
     this.email = Object.values(decodeToken)[1].email
     this.rol = Object.values(decodeToken)[1].rol.nombre
+    this.rol =  this.rol.charAt(0).toUpperCase() + this.rol.slice(1);
+    this.image = Object.values(decodeToken)[1].foto_usuario
+    this.image = "data:image/jpeg;base64," + this.image
   }
 
+  onSelectFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
 
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
 
-  private readFileAsString(file: File) {
-    return new Promise<string>(function(resolve, reject) {
-      let reader: FileReader = new FileReader();
-      reader.readAsDataURL(file)
-      reader.onload = function() {
-        resolve(this.result as string)
+      reader.onload = (event) => { // called once readAsDataURL is completed
+       this.image =  event.target!.result;
       }
-    })
-  }
-
-  public includeImageInItem(event: any): void {
-    const inputFile = event.target as HTMLInputElement;
-    const file: File | null = inputFile.files?.item(0) ?? null;
-
-
-    this.readFileAsString(file!).then(
-      (result) => {
-        const imageType: string = this.getImageType(result);
-        console.log(imageType);
-        const imageBase64: string = this.getImageBase64(result);
-        console.log(imageBase64);
-
-        this.image = imageBase64;
-
-      },
-      (error) => {
-        console.log("No se pudo cargar la imagen")
-      })
-        
-  }
-
-
-  private getImageType(imageString: string): string {
-    const imageStringParts: string[] = imageString.split(",");
-    if (imageStringParts.length == 2) {
-      return imageStringParts[0];
-    } else {
-      return "";
-    }
-  }
-
-
-  private getImageBase64(imageString: string): string {
-    const imageStringParts: string[] = imageString.split(",");
-    if (imageStringParts.length == 2) {
-      return imageStringParts[1];
-    } else {
-      return "";
     }
   }
 
