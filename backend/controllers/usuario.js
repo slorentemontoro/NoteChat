e366fdcd75c8ddfd62a3241546aa9061f764bc56
abeja21 <gestruiik@gmail.com>
2022-05-24 16:49:02 +0200

    sergio it's your time

--- backend/controllers/usuario.js
+++ backend/controllers/usuario.js
@@ -40,7 +40,7 @@ const login=async(nick,password)=>{
 }
 
 const getUsuarios = async() => {
-return await Usuario.find();
+return await Usuario.find().populate('rol');
 }
 
 const getUsuarioById=async(id)=>{
