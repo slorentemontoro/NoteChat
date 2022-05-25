b73bfb9f162154f68efadb96c668655a76f08115
nbustos21 <nicolasbustoos21@gmail.com>
2022-05-25 18:34:07 +0200

    Creation of privacy terms

---
+++ frontend/src/app/layouts/privacity/privacity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacity',
+  templateUrl: './privacity.component.html',
+  styleUrls: ['./privacity.component.scss']
+})
+export class PrivacityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
