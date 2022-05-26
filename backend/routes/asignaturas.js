4c57eadd021c7759aaae131d698cd1a3e7a91a08
abeja21 <gestruiik@gmail.com>
2022-05-26 17:38:45 +0200

    pls no merges

--- backend/routes/asignaturas.js
+++ backend/routes/asignaturas.js
@@ -5,13 +5,13 @@ var AsignaturasController=require('../controllers/asignaturas');
 
 router.get('/:idgrado',async(req,res)=>{
     const{idgrado}=req.params;
-    const result=AsignaturasController.obtenerLasAsignaturasDeUnGrado(idgrado);
+    const result=await AsignaturasController.obtenerLasAsignaturasDeUnGrado(idgrado);
     res.json(result);
 })
 
 router.post('/',async(req,res)=>{
 const {asignatura}=req.body;
-const result=AsignaturasController.crearUnaAsignatura(asignatura);
+const result=await AsignaturasController.crearUnaAsignatura(asignatura);
 res.json(result)
 })
 
