801a9f96bc2307d561e5a2ce6be6339249bf8757
nbustos21 <nicolasbustoos21@gmail.com>
2022-05-30 11:12:54 +0200

    map functionality

--- frontend/src/app/layouts/locations-list/map/map.component.ts
+++ frontend/src/app/layouts/locations-list/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationsService } from 'src/app/entities/location/service/locations.service';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map!: string
+  constructor(private locationService: LocationsService) { }
 
   ngOnInit(): void {
+  }
+
+  getLocations(){
+    this.locationService.getAllLocations().subscribe({
+      next: (data) => {
+        this.map = data.
+      },
+      error: (err) => {console.log(err)}
+    })
   }
 
 }
